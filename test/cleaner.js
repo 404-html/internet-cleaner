c71c68b2807f19a050ccfaf89b21aabbf944133d
roottool <smart.dog.lilienthal@gmail.com>
2019-04-15 06:27:00 +0900

    Wrote an unit test for Cleaner

---
+++ test/cleaner.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+
+import Consts from '../src/const';
+import Cleaner from '../src/cleaner';
+
+const niceWord = Consts.NICE_WORD;
+
+test('LowerCase', t => {
+    const badWord = 'fuck';
+
+    t.is(Cleaner.Clean(badWord, badWord, niceWord), niceWord.toLowerCase());
+});
+
+test('UpperCase', t => {
+    const badWord = 'FUCK';
+
+    t.is(Cleaner.Clean(badWord, badWord, niceWord), niceWord.toUpperCase());
+});
+
+test('Capitalized', t => {
+    const badWord = 'Fuck';
+    const capitalizedNiceWord = niceWord.charAt(0).toUpperCase() + niceWord.slice(1);
+
+    t.is(Cleaner.Clean(badWord, badWord, niceWord), capitalizedNiceWord);
+});
