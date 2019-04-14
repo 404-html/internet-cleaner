9ba6b53ab4a00fed00e8f114e2b53d6b361f4fb7
roottool <smart.dog.lilienthal@gmail.com>
2019-04-15 06:28:35 +0900

    Implemented a case sensitive logic in Cleaner

--- src/cleaner.js
+++ src/cleaner.js
@@ -1,5 +1,22 @@
 export default {
 	Clean(content, badWord, niceWord) {
+		/**
+		 * @description Make the first letter of a string uppercase
+		 * @link https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+		 */
+		const capitalizedBadWord = badWord.charAt(0).toUpperCase() + badWord.slice(1);
+
+		if (content === badWord.toLowerCase()) {
+			return niceWord.toLowerCase();
+		}
+		else if (content === badWord.toUpperCase()) {
+			return niceWord.toUpperCase();
+		}
+		else if (content === capitalizedBadWord) {
+			const capitalizedNiceWord = niceWord.charAt(0).toUpperCase() + niceWord.slice(1);
+			return capitalizedNiceWord;
+		}
+
 		return content.replace(new RegExp(badWord, 'ig'), niceWord);
 	}
 }
