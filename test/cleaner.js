493a3915cb3e2c0940bf63258d92c52926d8a9c6
Artur Drozdz <arturd@xara.com>
2019-04-15 19:30:49 +0200

    Internet cleaning of Internet Cleaner

--- test/cleaner.js
+++ test/cleaner.js
@@ -4,28 +4,25 @@ import { atob } from 'atob';
 import Consts from '../src/const';
 import Cleaner from '../src/cleaner';
 
-const base64BadWord = Consts.BAD_WORD;
+const badWord = 'fcuk';
 const niceWord = Consts.NICE_WORD;
 
 test('LowerCase', t => {
-    const testSentence = 'fuck fack fuck';
-    const badWord = atob(base64BadWord);
+    const testSentence = 'fcuk fack fcuk';
     const expectValue = 'butterfly fack butterfly'
 
     t.is(Cleaner.Clean(testSentence, badWord, niceWord), expectValue);
 });
 
 test('UpperCase', t => {
-    const testSentence = 'FUCK FACK FUCK';
-    const badWord = atob(base64BadWord);
+    const testSentence = 'FCUK FACK FCUK';
     const expectValue = 'BUTTERFLY FACK BUTTERFLY'
 
     t.is(Cleaner.Clean(testSentence, badWord, niceWord), expectValue);
 });
 
 test('Capitalized', t => {
-    const testSentence = 'Fuck Fack Fuck';
-    const badWord = atob(base64BadWord);
+    const testSentence = 'Fcuk Fack Fcuk';
     const expectValue = 'Butterfly Fack Butterfly'
 
     t.is(Cleaner.Clean(testSentence, badWord, niceWord), expectValue);
@@ -33,13 +30,11 @@ test('Capitalized', t => {
 
 test('No bad word', t => {
     const testWord = 'beautiful word';
-    const badWord = atob(base64BadWord);
-
     t.is(Cleaner.Clean(testWord, badWord, niceWord), testWord);
 });
 
 test('Bad word of base64', t => {
-    const badWord = atob(base64BadWord);
+    const decodedBadWord = atob(Consts.BAD_WORD);
 
-    t.is(Cleaner.Clean(badWord, badWord, niceWord), niceWord.toLowerCase());
+    t.is(Cleaner.Clean(decodedBadWord, decodedBadWord, niceWord), niceWord.toLowerCase());
 });
